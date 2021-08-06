5916afff66d33e375f00d9135625d17e7fe91907
ThalesCSoares <th4les.castro@gmail.com>
2021-08-06 04:39:06 -0300

    feat: criação do controller de auth

---
+++ parlador_api/src/app/controllers/AuthController.ts
@@ -0,0 +1,62 @@
+import{
+    Request,
+    Response,
+}from 'express';
+import{ getRepository } from 'typeorm';
+import bcrypt from'bcryptjs';
+
+import User  from '../models/User';
+
+import jwt from 'jsonwebtoken'; 
+
+interface NewUser{
+    id?:string;
+    name?:string;
+    email:string;
+    password?:string;
+    created_at:Date;
+}
+
+class AuthController{
+
+    async authenticate(req: Request, res: Response){
+
+        const repository = getRepository(User);
+
+        //verificando se email já existe
+        const {email, password} = req.body;
+
+        const user = await repository.findOne({ where: { email } });
+
+        if(!user){
+            res.status(401).json({message: "Unauthorized user" });
+            return;
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if(!isValidPassword){
+            res.status(401).json({message: "Unauthorized user" });
+            return;
+        }
+
+        //criando token de autenticação
+        const secret:string = process.env.SECRET!;
+        const token = jwt.sign({ id: user.id }, secret, { expiresIn:'1d' } );
+
+        //excluindo password para envio dos dados
+        const userData:NewUser = user; 
+        delete userData.password;
+
+        return res.status(201).json({
+            message:'Sucess login',
+            data:{
+                user: userData,
+                token:token,
+            }
+        });
+
+    }
+}
+
+export default new AuthController();
--- parlador_api/src/routes.ts
+++ parlador_api/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import UserController from "./app/controllers/UserController";
+import AuthController from "./app/controllers/AuthController";
 
 const router = Router();
 
 router.post('/users', UserController.store);
+router.post('/auth', AuthController.authenticate);
 
 //TESTE MIDDLEWARE
 //router.get('/users', authMiddleware,UserController.index);
